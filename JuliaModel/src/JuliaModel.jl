ee429b08cea16c9a872c648a7c4b78f4f7e52891
Remix23 <keru1000@gmail.com>
2026-05-03 13:03:51 +0200

    Parallelism in julia

--- JuliaModel/src/JuliaModel.jl
+++ JuliaModel/src/JuliaModel.jl
@@ -3,13 +3,34 @@ module JuliaModel
 import BeforeIT
 
 function run_simulation(parameters, initial_conditions, T)
+    if Threads.nthreads() > 1
+        parallel = true
+    else
+        parallel = false
+    end
     model = BeforeIT.Model(parameters, initial_conditions)
-    data = BeforeIT.run!(model, T)
+    data = BeforeIT.run!(model, T, parallel=parallel)
     gdp = data.data.real_gdp
     return gdp
 end
 
+function run_monte_carlo(parameters, initial_conditions, T, num_simulations)
+    models = [BeforeIT.Model(parameters, initial_conditions) for _ in 1:num_simulations]
+    data = BeforeIT.ensemblerun!(models, T, parallel=true)
+    gdps = [d.data.real_gdp for d in data]
+    gdp = mean(gdps, dims=1)
+    return gdp
+end
+
+function run_for_different_parameters(parameters_list, initial_conditions, T)
+    models = [BeforeIT.Model(parameters, initial_conditions) for parameters in parameters_list]
+    data = BeforeIT.ensemblerun!(models, T, parallel=true)
+    gdps = [d.data.real_gdp for d in data]
+    return gdps
+end
+
 function get_parameters() 
+    println(Threads.nthreads())
     return BeforeIT.AUSTRIA2010Q1.parameters
 end
 function get_initial_conditions()
--- calibration.py
+++ calibration.py
@@ -1,13 +1,29 @@
+from os import environ
+
+NUM_THREADS = 8
+
+environ["JULIA_NUM_THREADS"] = str(NUM_THREADS)
+environ["PYTHON_JULIACALL_HANDLE_SIGNALS"] = "yes"
+
 from juliacall import Main as jl
 
-jl.seval("using JuliaModel: run_simulation, get_parameters, get_initial_conditions")
+jl.seval("using JuliaModel: run_simulation, get_parameters, get_initial_conditions, run_for_different_parameters, run_monte_carlo")
 
 from sbi.utils import BoxUniform
 from sbi.inference import NPE
 import torch
 import numpy as np
 
+import matplotlib.pyplot as plt
+import pandas as pd
+from os import path
+from sys import argv
+
+from time import time
+
 ### for now, only real GDP
+CALIBRATION_DATE = "2010-01-01"
+T = 20
 
 def run_sim(parameters, initial_conditions, T):
     data = jl.run_simulation(parameters, initial_conditions, T)
@@ -16,11 +32,12 @@ def run_sim(parameters, initial_conditions, T):
 def compute_statistics (sim_out):
     # compute summary statistics from the simulated data
     # for example, we can compute the mean and standard deviation of real GDP
+
     mean_gdp = np.mean(sim_out)
     std_gdp = np.std(sim_out)
 
     ### yearly correlation y_t and y_t-4
-    yearly_corr = np.correlate(sim_out[:-4], sim_out[4:])
+    yearly_corr = np.corrcoef(sim_out[:-4], sim_out[4:])[0, 1]
     ### AR(1) coefficient of real GDP
     lagged = sim_out[:-1]
     target = sim_out[1:]
@@ -28,11 +45,10 @@ def compute_statistics (sim_out):
     ar1_coeff = ols[0]
     ### skewness of real GDP
     skewness_gdp = np.mean((sim_out - mean_gdp)**3) / std_gdp**3
-
     return np.array([mean_gdp, std_gdp, yearly_corr, ar1_coeff, skewness_gdp])
 ### of statistics
 n_stats = 5
-n_draws = 10
+n_draws = 100
 parameters = jl.get_parameters()
 initial_conditions = jl.get_initial_conditions()
 
@@ -60,23 +76,37 @@ for param, bounds in zip(parameters_to_calibrate, priors_bounds):
 priors_bounds = torch.tensor(priors_bounds, dtype=torch.float32)
 priors = BoxUniform(low=priors_bounds[:, 0], high=priors_bounds[:, 1])
 
-prior_draws = priors.sample((n_draws,))
-
 npe_x = np.ndarray(shape = (n_draws, n_stats))
 
-for i, draw in enumerate(prior_draws):
-    print("Drawn parameters:", draw)
-    sim_paramerters = parameters.copy()
-    for param, value in zip(parameters_to_calibrate, draw.tolist()):
-        print(f"  {param}: {value}")
-        sim_paramerters[param] = value
+### simulate data for NPE construction
+
+if path.exists("npe_data.npz") and len(argv) > 1 and argv[1] == "load":
+    print("Loading existing NPE data from file...")
+    data = np.load("npe_data.npz")
+    prior_draws = data["prior_draws"]
+    npe_x = data["npe_x"]
+else:
+    print("Generating new NPE data...")
+    prior_draws = priors.sample((n_draws,))
+    for i, draw in enumerate(prior_draws):
+        print("Drawn parameters:", draw)
+        sim_paramerters = parameters.copy()
+        for param, value in zip(parameters_to_calibrate, draw.tolist()):
+            # print(f"  {param}: {value}")
+            sim_paramerters[param] = value
+        
+        ### obtain simulated data
+        x = run_sim(sim_paramerters, initial_conditions, T)
+        # compute summary statistics
+        stats = compute_statistics(x)
+        # print(f"Summary statistics for draw {i}:", stats)
+        npe_x[i, :] = stats
     
-    ### obtain simulated data
-    x = run_sim(sim_paramerters, initial_conditions, 20)
-    # compute summary statistics
-    stats = compute_statistics(x)
-    # print(f"Summary statistics for draw {i}:", stats)
-    npe_x[i, :] = stats
+    ## save
+    np.savez("npe_data.npz", prior_draws=prior_draws.numpy(), npe_x=npe_x)
+
+print("finished")
+quit()
 
 ### masked auto-regressive flow (MAF) density estimator
 # elaborate on the choice of MAF and its advantages for this problem
@@ -85,3 +115,13 @@ inference.append_simulations(prior_draws, npe_x)
 inference.train()
 
 posterior = inference.build_posterior()
+
+### simulation based calibration
+
+
+### validation
+# quartile data
+real_gdp = pd.read_csv("data/austria_real_gdp_fred.csv", parse_dates=["observation_date"])
+real_gdp["observation_date"] = pd.to_datetime(real_gdp["observation_date"])
+real_gdp = real_gdp[real_gdp["observation_date"] >= CALIBRATION_DATE]
+real_gdp = real_gdp["CLVMNACSCAB1GQAT"].values
